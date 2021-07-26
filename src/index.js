93ceba03573efff1b1932995fec1780483bc9d72
Argen <aisakunov22@gmail.com>
2021-07-26 22:42:21 +0300

    Release0

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-import './sass/main.scss';
+'use strict';
+import menuCardTemplates from './templates/menu-card.handlebars';
+import menu from './menu.json';
+
+const menuNode = document.querySelector('.js-menu');
+const inpTheme = document.querySelector('#theme-switch-toggle');
+
+const cardsMenu = menuCardTemplates(menu);
+menuNode.insertAdjacentHTML('beforeend', cardsMenu);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+uploadTheme();
+
+inpTheme.addEventListener('change', () => {
+  const replacementЕtheme = document.body.classList.toggle(Theme.DARK);
+  localStorage.setItem('dark', replacementЕtheme);
+});
+
+function uploadTheme() {
+  const keyLocalStorage = localStorage.getItem('dark');
+  const parse = JSON.parse(keyLocalStorage);
+
+  if (parse) {
+    inpTheme.checked = true;
+    document.body.classList.add(Theme.DARK);
+  }
+}
